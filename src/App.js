322e481a19af8f7b4edb6cc3abe954e201981c17
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2019-11-25 19:54:12 -0800

    larn syntax

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import React,{Component} from 'react';
 
 class App extends Component {
   render(){
-    return <div><h1>Hello, world!</h1></div>;
+    return (
+	<React.Fragment>
+	 <label htmlFor="bar">bar</label>
+	 <input type="text" onChange={()=>{console.log("I am clicked")}}/>
+	</React.Fragment>
+    );
   }
 }
 export default App;

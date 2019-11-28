a00676fcf10e264c0d5d3d90db5d3b44c3b02938
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2019-11-27 17:30:38 -0800

    implement store

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -5,9 +5,9 @@ const initialState = {value: 0}
 export default (state = initialState, action) =>{
     switch (action.type){
         case INCREMENT:
-            return {state.value + 1}
+            return {value:state.value + 1}
         case DECREMENT:
-            return {state.value - 1}
+            return {value:state.value - 1}
         default:
             return state
     }

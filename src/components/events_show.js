0aa81eae0a551b4121ea8ac2f3fc91994f29f9ae
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2019-12-04 16:58:35 -0800

    implement delete feature

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,17 +2,23 @@ import React,{Component} from 'react';
 import { connect } from 'react-redux'
 import { Field, reduxForm} from 'redux-form'
 import { Link } from 'react-router-dom'
-import { format } from 'path';
-import { from } from 'rxjs';
-import { importDefaultSpecifier } from '@babel/types';
-import { __values } from 'tslib';
-import { postEvent } from '../actions'
 
-class EventsNew extends Component {
+import { getEvent, deleteEvent, putEvent } from '../actions'
+import { id } from 'postcss-selector-parser';
+
+class EventsShow extends Component {
   constructor(props){
     super(props)
     this.onSubmit = this.onSubmit.bind(this)
+    this.onDeleteClick = this.onDeleteClick.bind(this)
+
   }
+
+  componentDidMount(){
+    const {id} = this.props.match.params
+    if (id) this.props.getEvent(id)
+  }
+
   renderField(field){
     const {input, label, type, meta: {touched, error } } = field
 
@@ -24,14 +30,19 @@ class EventsNew extends Component {
     )
   }
 
+  async onDeleteClick(){
+    const {id} = this.props.match.params
+    await this.props.deleteEvent(id)
+    this.props.history.push("/")
+  }
+
   async onSubmit(values){
-    await this.props.postEvent(values)
+    await this.props.putEvent(values)
     this.props.history.push("/")
   }
 
   render(){
     const { handleSubmit,pristine,submitting,invalid　}= this.props
-    console.log(submitting)
   return(
     <form onSubmit={handleSubmit(this.onSubmit)}>   
       <div> <Field label="Title" name="title" type="text" component={this.renderField}/></div> 
@@ -40,6 +51,7 @@ class EventsNew extends Component {
       <div>
         <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
         <Link to="/" >Cancel</Link>
+        <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
       </div>
     </form>      
      )
@@ -55,10 +67,14 @@ const validate = values =>{
   return errors
 }
 
-const mapDispatchToProps =({ postEvent })
+const mapStateToProps = (state, ownProps) =>{
+  const event = state.events[ownProps.match.params.id]
+  return { initialValues: event, event }
+}
+const mapDispatchToProps =({ deleteEvent, getEvent, putEvent })
 
-export default connect (null, mapDispatchToProps)
-(reduxForm({ validate, form: 'eventNewForm'})(EventsNew)
+export default connect (mapStateToProps, mapDispatchToProps)
+(reduxForm({ validate, form: 'eventShowForm', enableReinitialize: true })(EventsShow)
 )
 
 
--- src/index.js
+++ src/index.js
@@ -4,23 +4,28 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { BrowserRouter, Route, Switch} from 'react-router-dom'
-
+import {composeWithDevTools} from 'redux-devtools-extension'
 import './index.css';
 import reducer from './reducers'
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
+import EventsShow from './components/events_show';
 import * as serviceWorker from './serviceWorker';
 import { format } from 'path';
-import { form } from 'rxjs';
+import { form, from } from 'rxjs';
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const enhancer =  process.env.NODE_ENV === 'development' ?
+composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
 <Provider store={store}>
     <BrowserRouter>
         <Switch>
-        <Route exact path="/events/new" component={EventsNew} />
+        <Route path="/events/new" component={EventsNew} />
+        <Route path="/events/:id" component={EventsShow} />
         <Route exact path="/" component={EventsIndex} />
+        <Route exact path="/events" component={EventsIndex} />
         </Switch>
     </BrowserRouter>
 </Provider>,
